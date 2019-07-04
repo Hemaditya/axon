54c13598ee1e34c33ad82d0e9af6760714cb4f40
Hemaditya Prasad <hemadityaprasad@gmail.com>
2019-07-04 12:46:04 +0530

    Commit wit comments

--- OpenBCI/app.py
+++ OpenBCI/app.py
@@ -1,3 +1,4 @@
+# This is for processing of data
 import pyOpenBCI as p #For pyOpenBCI
 import re # to match ttyUSB
 import os # to get files in /dev
@@ -9,6 +10,20 @@ from scipy.fftpack import fft
 from matplotlib import mlab
 
 class DataStream():
+	'''
+	port = USB port number ex: /dev/ttyUSB0	
+	daisy = daisy board
+	chunk_size = BCI gives a sample with 8 valuesi for 8 channels. Chunk size defines how many such 
+				samples to read in one iteration
+	b_times = referes to the size of buffer w.r.t the chunk size
+				if chunk size is 50, and b_times is 5, then buffer_size will be 5*50 = 250
+	n_channels = no.of channels on BCI
+	spec_analyse = no.of chunks to send to fft
+	NFFT = bin number for fft
+	filters =   future use
+	channels = future use
+	spectrogramWindow = buffer size for spectrogram
+	'''
 	def __init__(self,port=None,daisy=False,chunk_size=250,b_times=32,n_channels=8,spec_analyse=3, NFFT=512, filters=None, channels='all', spectrogramWindow=100):
 		if(port == None):
 			self.get_port()
@@ -22,28 +37,34 @@ class DataStream():
 		self.buffer_size = self.chunk_size*b_times
 		self.data_buffer = []
 		self.raw_buffer = np.zeros(shape=(self.buffer_size))
-		self.uVolts_per_count = (4.5)/24/(2**23-1)*1000000
+		self.uVolts_per_count = (4.5)/24/(2**23-1)*1000000 # scalar factor to convert raw data into real world signal data
 		if(channels == 'all'):
 			self.channels_to_process = [i for i in range(n_channels)]	
 		else:	
 			self.channels_to_process = [i for i in channels]
-		self.filter_outputs = {}
-		self.plot_buffer = {}
+		self.filter_outputs = {} # outputs of all filters 
+		self.plot_buffer = {} # The data that needs to be plotted
 		self.spec_analyse = spec_analyse
-		self.spec_True = 0
+		self.spec_True = 0 # to flag spectrogram plotting
 		self.NFFT = NFFT
-		self.currentChannel = -1
+		
 		self.prevFilterOutput = {}
 		# Plotting and filter buffers
+		# Initialization of filter_outputs 
 		self.filter_outputs['dc_offset'] = np.zeros(shape=(self.buffer_size))
 		self.filter_outputs['notch_filter'] = np.zeros(shape=(self.buffer_size))
 		self.filter_outputs['bandpass'] = np.zeros(shape=(self.buffer_size))
 		self.filter_outputs['spec_analyser'] = np.array([])
 		self.plot_buffer['dc_offset'] = np.array([])
 		self.plot_buffer['notch_filter'] = np.array([])
-
+		self.g = 0
+		self.plot_buffer['bandpass'] = np.array([])
+		self.plot_buffer['spec_analyser'] = np.zeros(shape=(self.window_size*self.chunk_size))
+		self.plot_buffer['spec_freqs'] = np.zeros(shape=(self.window_size*self.chunk_size))
+		self.plot_buffer['spectrogram'] = np.zeros(shape=(100,self.NFFT/2 + 1))
 
 	def read_chunk(self,n_chunks=1):
+		# n_chunks = number of chunks to read. Keep it 1 for live data
 		all_chunks = []
 		for i in range(n_chunks):
 			all_chunks.append(self.stream.start_stream(self.chunk_size))
@@ -59,69 +80,60 @@ class DataStream():
 
 
 	def notch_filter(self):
+		# This is to remove the AC mains noise interference	of frequency of 50Hz(India)
 		notch_freq_Hz = np.array([50.0])  # main + harmonic frequencies
 		for freq_Hz in np.nditer(notch_freq_Hz):  # loop over each target freq
 			bp_stop_Hz = freq_Hz + 3.0*np.array([-1, 1])  # set the stop band
 			b, a = signal.butter(3, bp_stop_Hz/(250 / 2.0), 'bandstop')
 			notchOutput	= signal.lfilter(b, a, self.filter_outputs['dc_offset'], 0)[-self.window_size:]
 			#self.plot_buffer['notch_filter'] = np.append(self.filter_outputs['notch_filter'],notchOutput)
+			# A = [6,7,8,9,1,2,3,4] = [7,8,9,1,2,3,4,4]
+			# A[last value] = notchOutput
+			# A[:num] = all values from start till index num
+			# A[:-num] = all values from start to index num from the revers
 			self.filter_outputs['notch_filter'][:-self.window_size] = self.filter_outputs['notch_filter'][self.window_size:]
 			self.filter_outputs['notch_filter'][-self.window_size:] = notchOutput
 
 	def bandpass(self):
+		# This is to allow the band of signal to pass with start frequency and stop frequency
 		start = 1
 		stop = 60
 		bp_Hz = np.zeros(0)
 		bp_Hz = np.array([start,stop])
 		b, a = signal.butter(3, bp_Hz/(250 / 2.0),'bandpass')
-		bandpassOutput = signal.lfilter(b, a, self.prevFilterOutput[self.currentChannel], 0)[-self.window_size:]
+		bandpassOutput = signal.lfilter(b, a, self.filter_outputs['notch_filter'], 0)[-self.window_size:]
 		#self.plot_buffer['bandpass'] = np.append(self.filter_outputs['bandpass'],bandpassOutput)
 		self.filter_outputs['bandpass'][:-self.window_size] = self.filter_outputs['bandpass'][self.window_size:]
 		self.filter_outputs['bandpass'][-self.window_size:] = bandpassOutput
-		self.prevFilterOutput[self.currentChannel] = np.copy(self.filter_outputs['bandpass'])
 
 	def remove_dc_offset(self):
-		hp_cutoff_Hz = 1.0
-
+		# This is to Remove The DC Offset By Using High Pass Filters
+		hp_cutoff_Hz = 1.0 # cuttoff freq of 1 Hz (from 0-1Hz all the freqs at attenuated)
 		b, a = signal.butter(2, hp_cutoff_Hz/(250 / 2.0), 'highpass')
-<<<<<<< HEAD
-		dcOutput = signal.lfilter(b, a, self.prevFilterOutput[self.currentChannel], 0)[-self.window_size:]
-		self.filter_outputs['dc_offset'][:-self.window_size] = self.filter_outputs['dc_offset'][self.window_size:]
-		self.filter_outputs['dc_offset'][-self.window_size:] = dcOutput
-		self.prevFilterOutput[self.currentChannel] = np.copy(self.filter_outputs['dc_offset'])
-=======
 		dcOutput = signal.lfilter(b, a, self.raw_buffer, 0)[-self.window_size:]
 		self.filter_outputs['dc_offset'][:-self.window_size] = self.filter_outputs['dc_offset'][self.window_size:]
 		self.filter_outputs['dc_offset'][-self.window_size:] = dcOutput
->>>>>>> parent of 86f6079... Added a prevFilterOutput variable which holds the output of the previous Filter. This will make it easier to disable filters and get to know its effect on the spectrogram
 
 	def get_spectrum_data(self):
+		# This is to generate spectrogram data
 		NFFT = 512
 		overlap  = NFFT - int(0.25 * 250)
-<<<<<<< HEAD
-		spec_PSDperHz, spec_freqs, spec_t  = mlab.specgram(np.squeeze(self.prevFilterOutput[self.currentChannel]),
-=======
 		spec_PSDperHz, spec_freqs, spec_t  = mlab.specgram(np.squeeze(self.filter_outputs['spec_analyser']),
->>>>>>> parent of 86f6079... Added a prevFilterOutput variable which holds the output of the previous Filter. This will make it easier to disable filters and get to know its effect on the spectrogram
 									   NFFT=NFFT,
 									   window=mlab.window_hanning,
 									   Fs=250,
 									   noverlap=overlap
 									   ) # returns PSD power per Hz
-		#print('PSDHERZ: ',spec_PSDperHz.shape)
-		#print('spec_freqs: ',spec_freqs.shape)
-		#print('t: ',spec_t.shape)
-		#x = raw_input("SAD")
 		spectrum_PSDperHz = np.mean(spec_PSDperHz,1)
-		self.plot_buffer['spec_analyser'][self.currentChannel] = np.copy(10*np.log10(spectrum_PSDperHz))
-		self.plot_buffer['spec_freqs'][self.currentChannel] = np.copy(spec_freqs)
+		self.plot_buffer['spec_analyser'] = np.copy(10*np.log10(spectrum_PSDperHz))
+		self.plot_buffer['spec_freqs'] = np.copy(spec_freqs)
 
 		#self.plot_buffer['spectrogram'][:,:-1] = self.plot_buffer['spectrogram'][:,1:]
-		self.plot_buffer['spectrogram'] = np.roll(self.plot_buffer['spectrogram'][self.currentChannel],-1,0)
+		self.plot_buffer['spectrogram'] = np.roll(self.plot_buffer['spectrogram'],-1,0)
 		self.plot_buffer['spectrogram'][-1:] = 10*np.log10(spectrum_PSDperHz).reshape(-1)
 		self.spec_True = 1
 
-	def process_raw(self,channels=[3],meth='live'):
+	def process_raw(self,channels=[0],meth='live'):
 		if(channels=='all'):
 			self.channels_to_process = [i+1 for i in range(self.n_channels)]
 			pass
@@ -129,16 +141,11 @@ class DataStream():
 			self.channels_to_process = [int(i) for i in channels]
 
 		for channel in self.channels_to_process:
-			self.currentChannel = channel
+			
 			for _n in range(self.data_buffer.shape[0]):
 				# shift window_size bytes from raw_buffer and add new bytes
 				self.raw_buffer[:-self.window_size] = self.raw_buffer[self.window_size:]
 				self.raw_buffer[-self.window_size:] = self.data_buffer[_n,:,channel]
-<<<<<<< HEAD
-				self.prevFilterOutput[self.currentChannel] = np.copy(self.raw_buffer)		
-=======
-				
->>>>>>> parent of 86f6079... Added a prevFilterOutput variable which holds the output of the previous Filter. This will make it easier to disable filters and get to know its effect on the spectrogram
 				# remove the dc offset from the raw_buffer data
 				self.remove_dc_offset()
 				#apply notch_filter
@@ -149,10 +156,6 @@ class DataStream():
 				self.filter_outputs['spec_analyser'] = np.append(self.filter_outputs['spec_analyser'], self.filter_outputs['bandpass'][-self.window_size:])
 				if(self.filter_outputs['spec_analyser'].reshape(-1).shape[0] == self.window_size*self.spec_analyse):
 					self.g= 1
-<<<<<<< HEAD
-					self.prevFilterOutput[self.currentChannel] = np.copy(self.filter_outputs['spec_analyser'])
-=======
->>>>>>> parent of 86f6079... Added a prevFilterOutput variable which holds the output of the previous Filter. This will make it easier to disable filters and get to know its effect on the spectrogram
 					self.get_spectrum_data()
 					self.filter_outputs['spec_analyser'] = np.array([])
 						
--- OpenBCI/plotter.py
+++ OpenBCI/plotter.py
@@ -1,3 +1,4 @@
+# This file is runs the main thread. Is responsible for plotting data
 import app
 import threading
 import numpy as np
@@ -5,46 +6,43 @@ import pyqtgraph as pg
 from pyqtgraph import QtCore, QtGui
 import time
 
-win = pg.GraphicsWindow(title='Plot')
-win.resize(600,400)
-view = win.addViewBox()
-#p1.setClipToView(True)
-#p1.setRange(xRange=[0,60])
-#p1.setRange(yRange=[-50,50])
-#curve = p1.plot(pen='y')
-#pos = np.array([0., 1., 0.5, 0.25, 0.75])
-#color = np.array([[0,255,255,255], [255,255,0,255], [0,0,0,255], (0, 0, 255, 255), (255, 0, 0, 255)], dtype=np.ubyte)
-##color = np.array([[0,0,255,255], [255,255,0,255], [0,0,0,255] ], dtype=np.ubyte)
-#cmap = pg.ColorMap(pos, color)
-#lut = cmap.getLookupTable(0.0, 1.0, 256)
 
+win = pg.GraphicsWindow(title='Plot')
+win.resize(1000,600)
+view = win.addViewBox()
+# Spectrogram Initialization
 pos = np.array([0., 1., 0.5, 0.25, 0.75])
 color = np.array([[0,255,255,255], [255,255,0,255], [0,0,0,255], (0, 0, 255, 255), (255, 0, 0, 255)], dtype=np.ubyte)
 cmap = pg.ColorMap(pos, color)
 lut = cmap.getLookupTable(0.0, 1.0, 256)
-#
-#        self.img.setLookupTable(lut)
-#        self.img.setLevels([-50,40])
 item = pg.ImageItem()
 view.addItem(item)
 item.setLookupTable(lut)
 item.setLevels([-50,40])
+# The below 4 lines are for plotting filter_outputs
+#p1.setClipToView(True)
+#p1.setRange(xRange=[0,60])
+#p1.setRange(yRange=[-50,50])
+#curve = p1.plot(pen='y')
 
-#$imv.setLookupTable(lut)
-#$imv.setLevels([-50,40])
-
+# Initialize the processing stream
 appObj = app.DataStream(chunk_size=50,b_times=8,spec_analyse=5)
+# The below function will be run by thread t1
 def runApp(count=None):
 	if(count == None):
 		while(True):
 			appObj.read_chunk()
 			appObj.process_raw()
 
+
+# A thread to start processing of data
 t1 = threading.Thread(target=runApp)
 t1.start()
 time.sleep(0.1)
 
+# Main plotting function
 def update():
+	# The below 4 lines are for plotting filter_outputs
 	#if(appObj.plot_buffer['spectral_analysis'].shape[0] == appObj.window_size * appObj.spec_analysis):
 	#	curve.setData(appObj.plot_buffer['spectral_analysis']
 	#if appObj.g == 1:
@@ -55,45 +53,7 @@ def update():
 		appObj.spec_True = 0
 	pass
 	
-##class SpectrogramWidget(pg.PlotWidget):
-#    read_collected = QtCore.pyqtSignal(np.ndarray)
-#    def __init__(self):
-#        super(SpectrogramWidget, self).__init__()
-#
-#        self.img = pg.ImageItem()
-#        self.addItem(self.img)
-#
-#        self.img_array = np.zeros((1000, 512/2+1))
-#
-#        # bipolar colormap
-#
-#        pos = np.array([0., 1., 0.5, 0.25, 0.75])
-#        color = np.array([[0,255,255,255], [255,255,0,255], [0,0,0,255], (0, 0, 255, 255), (255, 0, 0, 255)], dtype=np.ubyte)
-#        cmap = pg.ColorMap(pos, color)
-#        lut = cmap.getLookupTable(0.0, 1.0, 256)
-#
-#        self.img.setLookupTable(lut)
-#        self.img.setLevels([-50,40])
-#
-#        #freq = np.arange((CHUNKSZ/2)+1)/(float(CHUNKSZ)/FS)
-#        #yscale = 1.0/(self.img_array.shape[1]/freq[-1])
-#        #self.img.scale((1./FS)*CHUNKSZ, yscale)
-#
-#        self.setLabel('left', 'Frequency', units='Hz')
-#
-#        #self.show()
-#
-#	def update(self):
-#		#if(appObj.plot_buffer['spectral_analysis'].shape[0] == appObj.window_size * appObj.spec_analysis):
-#		#	curve.setData(appObj.plot_buffer['spectral_analysis']
-#		#if appObj.g == 1:
-#		#	curve.setData(appObj.plot_buffer['spec_freqs'],appObj.plot_buffer['spec_analyser'])
-#
-#		print("JAJAJAJ")
-#		if(appObj.spec_True == 1):
-#			self.img.setImage(appObj.plot_buffer['spectrogram'],autoLevels=False)
-#			appObj.spec_True = 0
-#		pass
+# PyQTgraph initialization
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
